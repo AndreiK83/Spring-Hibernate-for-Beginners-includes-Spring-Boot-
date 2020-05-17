82c7efcada5998376b8fe0f0cd7468fe16fc3732
Andrei Chiriac <andreichiriac435@yahoo.com>
2020-05-17 14:30:58 +0300

    commit

--- spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -3,8 +3,15 @@ package com.luv2code.springdemo;
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//		retrieve the bean from the container
+//		getBean method parameters -> 1 bean id / 2 interface.class
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+//		call the method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+//		close context
+		context.close();
 	}
 
 }
